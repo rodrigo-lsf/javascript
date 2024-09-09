5bd69205d8d1ede962d33034a6c75f3b2bdc5f13
Rodrigo Sena <154368936+rodrigo-lsf@users.noreply.github.com>
2024-09-09 19:35:29 -0300

    Fiz o exercício 17

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,16 @@
-alert('Olá!')
+function calcular() {
+    var numero = Number(document.getElementById('numero').value)
+    var tabuada = document.getElementById('tabuada')
+
+    tabuada.innerHTML = ''
+
+    if (numero == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        for (var c = 1; c <= 10; c++) {
+            var option = document.createElement('option')
+            option.text = `${numero} x ${c} = ${numero * c}`
+            tabuada.add(option)
+        }
+    }
+}
